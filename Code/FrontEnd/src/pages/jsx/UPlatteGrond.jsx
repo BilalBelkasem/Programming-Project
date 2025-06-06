bce09ca00b67ee28d9b2e361788e2140a8724855
Mrfoxgamer <118847164+Mrfoxgamer@users.noreply.github.com>
2025-06-06 11:14:42 +0200

    PlatteGrondPagina Gefixed
    css gefixed

--- Code/FrontEnd/src/pages/jsx/UPlatteGrond.jsx
+++ Code/FrontEnd/src/pages/jsx/UPlatteGrond.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo Erasmus.png";
 import plattegrondImg from "../../assets/plattegrond.png";
+import logo from "../../assets/logo Erasmus.png";
 import "../css/UPlatteGrond.css";
-
 
 export default function UPlatteGrond({ onLogout }) {
   const bedrijven = [
@@ -14,41 +13,52 @@ export default function UPlatteGrond({ onLogout }) {
   ];
 
   return (
-    <div className="pageWrapper UPlatteGrond">
+    <div className="pageWrapper">
       <header className="header">
         <img src={logo} alt="Erasmus Logo" className="logo" />
         <nav className="nav">
-          <Link to="/dashboard" className="navLink">info</Link>
-          <Link to="/bedrijven" className="navLink">bedrijven</Link>
-          <Link to="/plattegrond" className="navLink">plattegrond</Link>
-          <Link to="/favorieten" className="navLink">favorieten</Link>
-          <Link to="/dashboard" className="navLink">mijn profiel</Link>
+          <Link to="/dashboard" className="navLink">
+            info
+          </Link>
+          <Link to="/bedrijven" className="navLink">
+            bedrijven
+          </Link>
+          <Link to="/plattegrond" className="navLink">
+            plattegrond
+          </Link>
+          <Link to="/favorieten" className="navLink">
+            favorieten
+          </Link>
+          <Link to="/dashboard" className="navLink">
+            mijn profiel
+          </Link>
         </nav>
-        <div onClick={onLogout} className="logoutIcon" title="Uitloggen">⇦</div>
+        <div onClick={onLogout} className="logoutIcon" title="Uitloggen">
+          ⇦
+        </div>
       </header>
 
       <main className="main">
-        <div className="titleBox">Plattegrond</div>
+        <div className="contentWrapper">
+          <h1 className="title">Plattegrond</h1>
 
-        <div className="contentContainer">
-          <div className="imageContainer">
+          <div className="contentContainer">
             <img
               src={plattegrondImg}
-              alt="Plattegrond Career Launch"
-              className="plattegrondImage"
+              alt="Plattegrond van de Career Launch"
+              className="plattegrond-image"
             />
+            <aside className="legend">
+              <h3>Legende</h3>
+              <ul>
+                {bedrijven.map((bedrijf) => (
+                  <li key={bedrijf.id}>
+                    {bedrijf.id}. {bedrijf.naam}
+                  </li>
+                ))}
+              </ul>
+            </aside>
           </div>
-
-          <aside className="legend">
-            <h3>Legende</h3>
-            <ul>
-              {bedrijven.map((bedrijf) => (
-                <li key={bedrijf.id}>
-                  <strong>{bedrijf.id}.</strong> {bedrijf.naam}
-                </li>
-              ))}
-            </ul>
-          </aside>
         </div>
       </main>
     </div>
