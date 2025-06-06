585cb383141961f494062f8a497a3605ea1f8d33
ayman-boun <ayman.bounaanaa@student.ehb.be>
2025-06-06 11:26:18 +0200

    probleem oplosssing vite
    ik had witte scherm wanneer ik vite opende

--- Code/FrontEnd/src/App.jsx
+++ Code/FrontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
+// Pagina-imports
 import GInfoPagina from './pages/jsx/GInfoPagina.jsx';
 import UInfoPagina from './pages/jsx/UInfoPagina.jsx';
 import LoginPagina from './pages/jsx/LoginPagina.jsx';
@@ -8,7 +9,7 @@ import UBedrijven from './pages/jsx/UBedrijven.jsx';
 import AdminDashboard from './pages/jsx/AdminDashboard.jsx';
 import CompanyRegistrationForm from './pages/jsx/CompanyRegistrationForm.jsx';
 import ClientRegistration from './pages/jsx/ClientRegistration.jsx';
-import Profielbedrijven from './pages/jsx/ProfielBedrijven.jsx';
+import ProfielBedrijven from './pages/jsx/ProfielBedrijven.jsx';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,47 +19,33 @@ export default function App() {
       {/* Publieke startpagina */}
       <Route path="/" element={<GInfoPagina />} />
 
-      {/* Loginpagina met login handler */}
+      {/* Loginpagina */}
       <Route
         path="/login"
         element={<LoginPagina onLogin={() => setIsLoggedIn(true)} />}
       />
 
-      {/* Bedrijven registratiepagina */}
-      <Route
-        path="/bedrijf-registratie"
-        element={<CompanyRegistrationForm />}
-      />
-
-      {/* Gebruikersdashboard, enkel zichtbaar als ingelogd */}
-      <Route
-        path="/dashboard"
-        element={
-          isLoggedIn
-            ? <UInfoPagina onLogout={() => setIsLoggedIn(false)} />
-            : <GInfoPagina />
-        }
-      />
-
-      {/* Bedrijvenpagina voor ingelogde gebruikers */}
-      <Route
-        path="/bedrijven"
-        element={
-          isLoggedIn
-            ? <UBedrijven />
-            : <GInfoPagina />
-        }
-      />
+      {/* Publieke registratiepagina's */}
+      <Route path="/bedrijf-registratie" element={<CompanyRegistrationForm />} />
       <Route path="/registreer" element={<ClientRegistration />} />
 
-      <Route path="/Profielbedrijven" element={<Profielbedrijven />} />
+      {/* Profielpagina voor geregistreerd bedrijf (geen bescherming nodig tenzij gewenst) */}
+      <Route path="/profiel-bedrijf" element={<ProfielBedrijven />} />
 
-      <Route path="/Profielstudent" element={<Profielstudent />} />
+      {/* Gebruikersdashboard (alleen toegankelijk als ingelogd) */}
+      <Route
+        path="/dashboard"
+        element={isLoggedIn ? <UInfoPagina onLogout={() => setIsLoggedIn(false)} /> : <Navigate to="/login" />}
+      />
 
-      {/* Admin dashboard – beveiliging optioneel */}
+      {/* Bedrijvenpagina (alleen toegankelijk als ingelogd) */}
+      <Route
+        path="/bedrijven"
+        element={isLoggedIn ? <UBedrijven /> : <Navigate to="/login" />}
+      />
+
+      {/* Admin dashboard */}
       <Route path="/admin" element={<AdminDashboard />} />
-
-
     </Routes>
   );
 }
