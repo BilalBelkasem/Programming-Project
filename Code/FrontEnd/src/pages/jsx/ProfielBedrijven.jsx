b34389054fe7283449c2ea0ce8e4fab7b868a417
ayman-boun <ayman.bounaanaa@student.ehb.be>
2025-06-10 10:58:35 +0200

    verbeteringen bedrijven registratie
    -links tussen paginas
    -melding bij het niet invullen van de gevraagde componenten

--- Code/FrontEnd/src/pages/jsx/ProfielBedrijven.jsx
+++ Code/FrontEnd/src/pages/jsx/ProfielBedrijven.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Css/ProfielBedrijven.css';
-
+import { Link } from 'react-router-dom';
 export default function CompanyProfilePage() {
 
   const company = {
--- Code/FrontEnd/src/pages/jsx/ProfielStudent.jsx
+++ Code/FrontEnd/src/pages/jsx/ProfielStudent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Css/Profielstudent';
+import { Link } from 'react-router-dom';
 
 export default function StudentProfilePage() {
   const student = {
