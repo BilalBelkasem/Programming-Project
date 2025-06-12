83dd95a10958b7b2aee65e8904863662384fc915
neptune <Bilal.belkasem@student.ehb.be>
2025-06-12 11:46:14 +0200

    Bedrijf registratie system not tested yet

--- Career_launch_2026 (2).sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS Likes (
-  User_ID INT(11) NOT NULL,
-  ID_target INT(11) NOT NULL,
-  target_type VARCHAR(50) NOT NULL,
-  PRIMARY KEY (User_ID, ID_target, target_type)
-);
