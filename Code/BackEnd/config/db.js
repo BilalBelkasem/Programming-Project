84344c84fbef14208c7ec68701e28eb3d4e5e6b0
ayman-boun <ayman.bounaanaa@student.ehb.be>
2025-06-16 13:44:28 +0200

    bug fix (nog ni klaar)

--- Code/BackEnd/config/db.js
+++ Code/BackEnd/config/db.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql2/promise'); // gebruik promise-versie
-require('dotenv').config();
+  const mysql = require('mysql2/promise'); // gebruik promise-versie
+  require('dotenv').config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+  const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
 
-module.exports = db;
+  module.exports = db;
