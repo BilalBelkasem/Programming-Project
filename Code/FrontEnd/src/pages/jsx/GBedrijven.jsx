4cf1a355ceaace3a85926fbd8d654d775e7753aa
DenisBuj <denis.bujorean@student.ehb.be>
2025-06-18 10:15:00 +0200

    Update GBedrijven.jsx

--- Code/FrontEnd/src/pages/jsx/GBedrijven.jsx
+++ Code/FrontEnd/src/pages/jsx/GBedrijven.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo Erasmus.png';
-import '../css/UBedrijven.css';
+import '../css/Gbedrijveninfopagina.css';
 
 export default function UBedrijven({ onLogout }) {
   const navigate = useNavigate();
